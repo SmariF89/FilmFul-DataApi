8a0c88b287cef0a85ce7a7ae3b6c28e7fc09f573
clover89 <smarifreyr30@gmail.com>
2020-02-04 10:54:58 +0000

    Initial commit. Project basis is set up.

---
+++ FilmFul_API.Repositories/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FilmFul_API.Repositories
+{
+    public class Class1
+    {
+    }
+}
---
+++ FilmFul_API.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FilmFul_API.Services
+{
+    public class Class1
+    {
+    }
+}
