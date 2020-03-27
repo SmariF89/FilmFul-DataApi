b9cbd5242a7621e07661674cd522b53495edd90d
clover89 <smarifreyr30@gmail.com>
2020-03-27 00:15:35 +0000

    Implmented: GetAllDirectors, GetDirectorById and GetDirectorActorsByDirectorId. LAST COMMIT WAS ACCIDENTALLY ONLY FOR Api

---
+++ FilmFul_API.Services/DirectorService.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using FilmFul_API.Models.Dtos;
+using FilmFul_API.Repositories;
+
+namespace FilmFul_API.Services
+{
+    public class DirectorService
+    {
+        private readonly DirectorRepository directorRepository = new DirectorRepository();
+
+        public (IEnumerable<DirectorDto>, int) GetAllDirectors(int pageSize, int pageIndex)
+        {
+            return directorRepository.GetAllDirectors(pageSize, pageIndex);
+        }
+
+        public DirectorDto GetDirectorById(int id)
+        {
+            return directorRepository.GetDirectorById(id);
+        }
+
+        public IEnumerable<ActorDto> GetDirectorActorsByDirectorId(int id)
+        {
+            return directorRepository.GetDirectorActorsByDirectorId(id);
+        }
+    }
+}
